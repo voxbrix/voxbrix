5b2bb846145651e9983e9792535589f70c26aa85
Daniil Nemtsev <nmsv@pm.me>
2023-01-26 23:18:49 +0000

    Server: update redb, add type checking for tables

--- voxbrix_server/src/entity/player.rs
+++ voxbrix_server/src/entity/player.rs
@@ -1,9 +1,21 @@
-use crate::storage::AsKey;
+use crate::storage::{
+    DataSized,
+    StoreSized,
+    UnstoreError,
+};
+use redb::{
+    RedbKey,
+    RedbValue,
+    TypeName,
+};
 use serde::{
     Deserialize,
     Serialize,
 };
-use std::mem;
+use std::{
+    cmp::Ordering,
+    mem,
+};
 use voxbrix_common::pack::PackDefault;
 
 pub const KEY_LENGTH: usize = mem::size_of::<usize>();
@@ -11,28 +23,53 @@ pub const KEY_LENGTH: usize = mem::size_of::<usize>();
 #[derive(Serialize, Deserialize, PartialOrd, Ord, PartialEq, Eq, Clone, Copy, Debug)]
 pub struct Player(pub usize);
 
-impl AsKey<KEY_LENGTH> for Player {
-    fn write_key(&self, buf: &mut [u8]) {
-        buf.copy_from_slice(&(self.0).to_be_bytes());
+impl PackDefault for Player {}
+
+impl StoreSized<KEY_LENGTH> for Player {
+    fn store_sized(&self) -> DataSized<Self, KEY_LENGTH> {
+        DataSized::new(self.0.to_be_bytes())
     }
 
-    fn read_key<B>(buf: B) -> Self
-    where
-        Self: Sized,
-        B: AsRef<[u8]>,
-    {
-        let buf: &[u8] = buf.as_ref();
-
-        Self(usize::from_be_bytes(buf.try_into().unwrap()))
-    }
-
-    fn to_key(self) -> [u8; Self::KEY_LENGTH] {
-        self.0.to_be_bytes()
-    }
-
-    fn from_key(from: [u8; Self::KEY_LENGTH]) -> Self {
-        Player(usize::from_be_bytes(from))
+    fn unstore_sized(stored: DataSized<Self, KEY_LENGTH>) -> Result<Self, UnstoreError> {
+        Ok(Self(usize::from_be_bytes(stored.data)))
     }
 }
 
-impl PackDefault for Player {}
+impl<'a> RedbValue for DataSized<Player, KEY_LENGTH> {
+    type AsBytes<'b> = &'b [u8]
+    where
+        Self: 'b;
+    type SelfType<'b> = DataSized<Player, KEY_LENGTH>
+    where
+        Self: 'b;
+
+    const ALIGNMENT: usize = 1usize;
+
+    fn fixed_width() -> Option<usize> {
+        None
+    }
+
+    fn from_bytes<'b>(data: &'b [u8]) -> Self::SelfType<'b>
+    where
+        Self: 'b,
+    {
+        DataSized::new(data.try_into().unwrap())
+    }
+
+    fn as_bytes<'b, 'c: 'b>(value: &'b Self::SelfType<'c>) -> Self::AsBytes<'b>
+    where
+        Self: 'b + 'c,
+    {
+        &value.data
+    }
+
+    fn type_name() -> TypeName {
+        TypeName::new("Player")
+    }
+}
+
+impl RedbKey for DataSized<Player, KEY_LENGTH> {
+    fn compare(data1: &[u8], data2: &[u8]) -> Ordering {
+        data1.cmp(data2)
+    }
+}
