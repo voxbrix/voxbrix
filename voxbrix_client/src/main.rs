e3489e0398f4faa29e16f5e84fa65be3ecdfd3e2
Daniil Nemtsev <nmsv@pm.me>
2023-03-01 16:34:50 +0000

    Client: exit on any panic, correct cleanup in case of normal shutdown of main async event loop

--- voxbrix_client/src/main.rs
+++ voxbrix_client/src/main.rs
@@ -3,6 +3,8 @@ use futures_lite::future;
 use log::error;
 use scene::MainScene;
 use std::{
+    panic,
+    process,
     thread,
     time::Duration,
 };
@@ -31,77 +33,86 @@ pub struct RenderHandle {
 fn main() {
     env_logger::init();
 
+    let default_panic = panic::take_hook();
+    panic::set_hook(Box::new(move |panic_info| {
+        default_panic(panic_info);
+        process::exit(1);
+    }));
+
     let (window_tx, window_rx) = flume::bounded::<WindowHandle>(1);
     let (event_proxy_tx, event_proxy_rx) = flume::bounded::<EventLoopProxy<WindowCommand>>(1);
 
     thread::spawn(move || {
-        let result = std::panic::catch_unwind(move || {
-            let rt = Box::leak(Box::new(LocalExecutor::new()));
-            let main_loop_rt = &rt;
-            future::block_on(rt.run(async move {
-                match window_rx.recv_async().await {
-                    Err(_) => {
-                        error!("unable to receive window handle");
-                    },
-                    Ok(window_handle) => {
-                        // Mailbox (Fast Vsync) and Immediate (No Vsync) work best with
-                        // the current rendering approach
-                        // Vulkan supports Mailbox present mode reliably and is cross-platform
-                        // https://github.com/gfx-rs/wgpu/issues/2128
-                        let instance = wgpu::Instance::new(wgpu::Backends::VULKAN);
-                        let surface = unsafe { instance.create_surface(&window_handle.window) };
+        let rt = &Box::leak(Box::new(LocalExecutor::new()));
+        future::block_on(rt.run(async move {
+            match window_rx.recv_async().await {
+                Err(_) => {
+                    error!("unable to receive window handle");
+                },
+                Ok(window_handle) => {
+                    // Mailbox (Fast Vsync) and Immediate (No Vsync) work best with
+                    // the current rendering approach
+                    // Vulkan supports Mailbox present mode reliably and is cross-platform
+                    // https://github.com/gfx-rs/wgpu/issues/2128
+                    let instance = wgpu::Instance::new(wgpu::Backends::VULKAN);
+                    let surface = unsafe { instance.create_surface(&window_handle.window) };
 
-                        let adapter = instance
-                            .request_adapter(&wgpu::RequestAdapterOptions {
-                                power_preference: wgpu::PowerPreference::LowPower,
-                                compatible_surface: Some(&surface),
-                                force_fallback_adapter: false,
-                            })
-                            .await
-                            .unwrap();
+                    let adapter = instance
+                        .request_adapter(&wgpu::RequestAdapterOptions {
+                            power_preference: wgpu::PowerPreference::LowPower,
+                            compatible_surface: Some(&surface),
+                            force_fallback_adapter: false,
+                        })
+                        .await
+                        .unwrap();
 
-                        let (device, queue) = adapter
-                            .request_device(
-                                &wgpu::DeviceDescriptor {
-                                    features: wgpu::Features::TEXTURE_BINDING_ARRAY
-                                        | wgpu::Features::SAMPLED_TEXTURE_AND_STORAGE_BUFFER_ARRAY_NON_UNIFORM_INDEXING,
-                                    // WebGL doesn't support all of wgpu's features, so if
-                                    // we're building for the web we'll have to disable some.
-                                    limits: if cfg!(target_arch = "wasm32") {
-                                        wgpu::Limits::downlevel_webgl2_defaults()
-                                    } else {
-                                        wgpu::Limits::default()
-                                    },
-                                    label: None,
+                    let (device, queue) = adapter
+                        .request_device(
+                            &wgpu::DeviceDescriptor {
+                                features: wgpu::Features::TEXTURE_BINDING_ARRAY
+                                    | wgpu::Features::SAMPLED_TEXTURE_AND_STORAGE_BUFFER_ARRAY_NON_UNIFORM_INDEXING,
+                                // WebGL doesn't support all of wgpu's features, so if
+                                // we're building for the web we'll have to disable some.
+                                limits: if cfg!(target_arch = "wasm32") {
+                                    wgpu::Limits::downlevel_webgl2_defaults()
+                                } else {
+                                    wgpu::Limits::default()
                                 },
-                                None, // Trace path
-                            )
-                            .await
-                            .unwrap();
+                                label: None,
+                            },
+                            None, // Trace path
+                        )
+                        .await
+                        .unwrap();
 
-                        let window_handle = Box::leak(Box::new(window_handle));
+                    let window_handle = Box::leak(Box::new(window_handle));
 
-                        let render_handle = Box::leak(Box::new(RenderHandle {
-                            surface,
-                            adapter,
-                            device,
-                            queue,
-                        }));
-                        let main_loop = MainScene {
-                            rt: main_loop_rt,
-                            window_handle,
-                            render_handle,
-                        };
-                        if let Err(err) = main_loop.run().await {
-                            error!("main_loop ended with error: {:?}", err);
-                        }
-                    },
-                }
-            }))
-        });
+                    let render_handle = Box::leak(Box::new(RenderHandle {
+                        surface,
+                        adapter,
+                        device,
+                        queue,
+                    }));
+                    let main_loop = MainScene {
+                        rt,
+                        window_handle,
+                        render_handle,
+                    };
+                    if let Err(err) = main_loop.run().await {
+                        error!("main_loop ended with error: {:?}", err);
+                    }
+                },
+            }
+        }));
 
-        if let Err(err) = result {
-            error!("main_loop panicked: {:?}", err);
+        // Cleanup to prevent tasks from being aborted instead of dropped correctly
+        // when the main thread exits
+        // WARNING: no task should be .detach() to run endlessly
+        // prefer to avoid .detach() altogether
+        // just have a handle around to be dropped
+        // at the end of the scope (that effectively drops the task)
+        while !rt.is_empty() {
+            rt.try_tick();
         }
 
         match event_proxy_rx.recv() {
