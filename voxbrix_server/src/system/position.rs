07dad64b3077a29cc8fd04f70208b67c2bc90aad
Daniil Nemtsev <nmsv@pm.me>
2025-09-23 22:18:02 +0000

    Server: make player actors produce PositionChange without applying it

--- voxbrix_server/src/system/position.rs
+++ voxbrix_server/src/system/position.rs
@@ -51,48 +51,48 @@ impl PositionSystemData<'_> {
     pub fn run(self) {
         let dt = self.process_timer.elapsed();
 
-        let par_iter = self
-            .velocity_ac
-            .par_iter()
-            .filter(|(actor, _)| self.player_ac.get(actor).is_none())
-            .filter_map(|(actor, velocity)| {
-                let position = self.position_ac.get(&actor)?;
-                let actor_class = self.class_ac.get(&actor)?;
-                let radius = match self.block_collision_acc.get(&actor_class, &actor)? {
-                    BlockCollision::AABB { radius_blocks } => radius_blocks,
-                };
+        let par_iter = self.velocity_ac.par_iter().filter_map(|(actor, velocity)| {
+            let position = self.position_ac.get(&actor)?;
+            let actor_class = self.class_ac.get(&actor)?;
+            let radius = match self.block_collision_acc.get(&actor_class, &actor)? {
+                BlockCollision::AABB { radius_blocks } => radius_blocks,
+            };
 
-                let mut collides_with_block = false;
+            let mut collides_with_block = false;
 
-                let (next_pos, next_vel) = position::process_actor(
-                    dt,
-                    self.class_bc,
-                    self.collision_bcc,
-                    &position,
-                    velocity,
-                    &radius,
-                    |_, _| {},
-                    |_, _| {
-                        collides_with_block = true;
-                    },
-                );
+            let (next_pos, next_vel) = position::process_actor(
+                dt,
+                self.class_bc,
+                self.collision_bcc,
+                &position,
+                velocity,
+                &radius,
+                |_, _| {},
+                |_, _| {
+                    collides_with_block = true;
+                },
+            );
 
-                // TODO only add if the actor has collision component
-                // AND insert any static (no velocity component) actors
-                //     that have collision component before using
-                Some(Change {
-                    actor,
-                    prev_position: *position,
-                    next_position: next_pos,
-                    prev_velocity: *velocity,
-                    next_velocity: next_vel,
-                    collides_with_block,
-                })
-            });
+            // TODO only add if the actor has collision component
+            // AND insert any static (no velocity component) actors
+            //     that have collision component before using
+            Some(Change {
+                actor,
+                prev_position: *position,
+                next_position: next_pos,
+                prev_velocity: *velocity,
+                next_velocity: next_vel,
+                collides_with_block,
+            })
+        });
 
         self.position_changes.from_par_iter(par_iter);
 
-        for change in self.position_changes.iter() {
+        for change in self
+            .position_changes
+            .iter()
+            .filter(|change| self.player_ac.get(&change.actor).is_none())
+        {
             self.position_ac
                 .insert(change.actor, change.next_position, *self.snapshot);
             self.velocity_ac
